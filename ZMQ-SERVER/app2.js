f20c8421b12da745acd5fa30ba714b24335da14d
Nando Pacheco <contato@nandopacheco.com>
2019-09-09 02:17:08 -0300

    t

--- ZMQ-SERVER/app2.js
+++ ZMQ-SERVER/app2.js
@@ -2,7 +2,7 @@ var zmq = require("zeromq"),
   sock = zmq.socket("sub");
 
 sock.connect("tcp://127.0.0.1:2004");
-sock.subscribe("");
+// sock.subscribe("");
 console.log("Subscriber connected to port 2004");
 
 sock.on("message", function(topic, message) {
